d677904fc2b77c9bac36adc3a197a95bb9b9f72a
Shoshinaiki <ptinard11@gmail.com>
2023-09-24 19:39:54 +0200

    fin de projet

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const user = require("../controllers/user.ctrl");
-const authCtrl = require("../controllers/auth.ctrl");
 const auth = require("../middleware/auth.js");
 const admin = require("../middleware/admin");
 
-router.post("/signup", authCtrl.signup);
-router.post("/login", authCtrl.login);
+router.post("/signup", user.signup);
+router.post("/login", user.login);
 router.get("/:id", auth, user.findOneUser);
 router.get("/", auth, admin, user.findAllUsers);
 router.delete("/:id", auth, user.deleteMyAccount);
